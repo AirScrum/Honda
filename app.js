34ec0374ba312db3d0c5c1de37f9ff209dacd6b0
mazen-mahmoud16 <mazenmahmoudabdelhafeez@gmail.com>
2023-03-04 04:07:45 +0200

    Implement changing Speech to text

--- app.js
+++ app.js
@@ -2,64 +2,52 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
-const { Deepgram } = require('@deepgram/sdk');
-const fs = require('fs');
+const { Deepgram } = require("@deepgram/sdk");
 
 // Instances
 const app = express();
 
+const mimetype = 'audio/mpeg';
+
 // Middlewares
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 
 // The API key you created in step 1
 const deepgramApiKey = process.env.DEEP_GRAM_API_KEY;
 
-// Replace with your file path and audio mimetype
-const pathToFile = 'D:/SEMESTER 9/CSE 491 Graduation Project 1/Grad Test Files/Honda/Honda/_- reading001.mp3';
-const mimetype = 'audio/mpeg';
-
 // Initializes the Deepgram SDK
 const deepgram = new Deepgram(deepgramApiKey);
 
-//To connect to database
-const dbURI = process.env.MONGO_DB_URI;
-
 mongoose
-  .connect(process.env.MONGO_DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then((result) => {
-    app.listen(process.env.PORT || 4001);
-    console.log(`Example app listening on port ${process.env.PORT}`);
-  })
-  .catch((err) => console.log(err));
-
+    .connect(process.env.MONGO_DB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then((result) => {
+        app.listen(process.env.PORT || 4001);
+        console.log(`Example app listening on port ${process.env.PORT}`);
+    })
+    .catch((err) => console.log(err));
 
 // Proxy request
-app.get("/", (req, res) => {
+app.post("/request/speech2text", (req, res) => {
 
-    deepgram.transcription.preRecorded(
-        { buffer: fs.readFileSync(pathToFile), mimetype },
-        { punctuate: true, language: 'en-US' },
-      )
-      .then((transcription) => {
-        console.dir(transcription, {depth: null});
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+  const audioBuffer = Buffer.from(req.body.buffer.data, 'binary');
+    deepgram.transcription
+        .preRecorded(
+            { buffer: audioBuffer, mimetype },
+            { punctuate: true, language: "en-US" }
+        )
+        .then((transcription) => {
+            console.dir(transcription, { depth: null });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 
-  return res.status(200).send({
-    success: "true"
-  })
-});
-
-
-//Route the request to the Speech2Text service
-app.get("/request/speech2text", (req, res) => {
     return res.status(200).send({
-        success: "true"
-      })
+        success: "true",
+    });
 });
+
