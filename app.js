bb3f839306ca252d5a5d537556f9b5a12556d20d
mazen-mahmoud16 <mazenmahmoudabdelhafeez@gmail.com>
2023-03-07 01:58:46 +0200

    Remove .then and .catch and use async and await in converting speech to text

--- app.js
+++ app.js
@@ -32,58 +32,46 @@ mongoose
     })
     .catch((err) => console.log(err));
 
-
 // Transform from speech to text
-app.post("/request/speech2text", (req, res) => {
-
+app.post("/request/speech2text", async (req, res) => {
     // Take user id
     const userid = req.body.userid;
 
     // Use deepgram to convert from audio to text
     const audioBuffer = Buffer.from(req.body.buffer.data, "binary");
-    deepgram.transcription
-        .preRecorded(
+
+    try {
+        const transcription = await deepgram.transcription.preRecorded(
             { buffer: audioBuffer, mimetype },
             { punctuate: true, language: "en-US" }
-        )
-        .then((transcription) => {
+        );
 
-            // Extract the transcript
-            const extractedText =
-                transcription.results.channels[0].alternatives[0].transcript;
+        // Extract the transcript
+        const extractedText =
+            transcription.results.channels[0].alternatives[0].transcript;
 
-            // Save to database
-            const text = new TextModel({
-                textContent: extractedText,
-                userID: userid,
-            });
-
-            // Save instance to mongoDB
-            text.save()
-                .then((text) => {
-                    return res.status(200).send({
-                        success: true,
-                        message: "Text created successfully.",
-                        text: {
-                            id: text._id,
-                            userID: text.userID,
-                        },
-                    });
-                })
-                .catch((err) => {
-                    return res.status(500).send({
-                        success: false,
-                        message: "Something went wrong",
-                        error: err,
-                    });
-                });
-        })
-        .catch((err) => {
-            res.status(500).send({
-                success: false,
-                message: "Something went wrong",
-                error: err,
-            });
+        // Save to database
+        const text = new TextModel({
+            textContent: extractedText,
+            userID: userid,
         });
-});
 
+        // Save instance to mongoDB
+        const textSaved = await text.save();
+
+        return res.status(200).send({
+            success: true,
+            message: "Text created successfully.",
+            text: {
+                id: textSaved._id,
+                userID: textSaved.userID,
+            },
+        });
+    } catch (err) {
+        res.status(500).send({
+            success: false,
+            message: "Something went wrong",
+            error: err,
+        });
+    }
+});
